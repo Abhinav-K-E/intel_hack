333d15ea5037a49b442f73ea29e77d6d4a2c5ba6
Abhinav <abhinavke091@gmail.com>
2024-03-23 13:21:39 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='wrapper'>
+      <div className='grid-left'>
+        <div className="main-txt">
+          Enter something
+        </div>
+        <div className="sub-txt">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod pariatur quam labore sapiente aperiam tempora! Iure ducimus error ex sapiente magnam, delectus iste nesciunt quos repudiandae, doloremque ipsam, harum architecto?
+        </div>
+      </div>
+      <div className='grid-right'>right</div>
+    </div>
+  );
+};
+
+export default App;
