9619c92851b6490614839b29bedcd9f8abb93c35
Abhinav <abhinavke091@gmail.com>
2024-03-23 13:37:13 +0530

    check-box-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,106 @@
 import React from 'react';
-import './App.css'
+import './App.css';
 
 const App = () => {
   return (
     <div className='wrapper'>
       <div className='grid-left'>
-        <div className="main-txt">
-          Enter something
-        </div>
-        <div className="sub-txt">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod pariatur quam labore sapiente aperiam tempora! Iure ducimus error ex sapiente magnam, delectus iste nesciunt quos repudiandae, doloremque ipsam, harum architecto?
+        <div className='main-txt'>Enter something</div>
+        <div className='sub-txt'>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod
+          pariatur quam labore sapiente aperiam tempora! Iure ducimus error ex
+          sapiente magnam, delectus iste nesciunt quos repudiandae, doloremque
+          ipsam, harum architecto?
         </div>
       </div>
-      <div className='grid-right'>right</div>
+      <div className='grid-right'>
+        <div className='upload-card'>
+          <div className='upload-preview'></div>
+          <div className='check-boxs'>
+            <div className='check-grp'>
+              <label className='container'>
+                <input defaultChecked='checked' type='checkbox' />
+                <div className='checkmark' />
+              </label>
+              <div className="check-name">
+                check 1
+              </div>
+            </div>
+
+            <div className='check-grp'>
+              <label className='container'>
+                <input defaultChecked='checked' type='checkbox' />
+                <div className='checkmark' />
+              </label>
+              <div className="check-name">
+                check 1
+              </div>
+            </div>
+
+            <div className='check-grp'>
+              <label className='container'>
+                <input defaultChecked='checked' type='checkbox' />
+                <div className='checkmark' />
+              </label>
+              <div className="check-name">
+                check 1
+              </div>
+            </div>
+
+            <div className='check-grp'>
+              <label className='container'>
+                <input defaultChecked='checked' type='checkbox' />
+                <div className='checkmark' />
+              </label>
+              <div className="check-name">
+                check 1
+              </div>
+            </div>
+
+            <div className='check-grp'>
+              <label className='container'>
+                <input defaultChecked='checked' type='checkbox' />
+                <div className='checkmark' />
+              </label>
+              <div className="check-name">
+                check 1
+              </div>
+            </div>
+
+            <div className='check-grp'>
+              <label className='container'>
+                <input defaultChecked='checked' type='checkbox' />
+                <div className='checkmark' />
+              </label>
+              <div className="check-name">
+                check 1
+              </div>
+            </div>
+
+
+            <div className='check-grp'>
+              <label className='container'>
+                <input defaultChecked='checked' type='checkbox' />
+                <div className='checkmark' />
+              </label>
+              <div className="check-name">
+                check 1
+              </div>
+            </div>
+
+            <div className='check-grp'>
+              <label className='container'>
+                <input defaultChecked='checked' type='checkbox' />
+                <div className='checkmark' />
+              </label>
+              <div className="check-name">
+                check 1
+              </div>
+            </div>
+          </div>
+          <div className='upload-btn'>Upload</div>
+        </div>
+      </div>
     </div>
   );
 };
